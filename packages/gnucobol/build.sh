17c98777d63df6efac1a3b1c30eeb00b33924404
abraham sofer <alexbodn@gmail.com>
2024-07-27 20:27:34 +0200

    only termux related patches

--- packages/gnucobol/build.sh
+++ packages/gnucobol/build.sh
@@ -3,19 +3,14 @@ TERMUX_PKG_DESCRIPTION="A free/libre COBOL compiler"
 TERMUX_PKG_LICENSE="GPL-3.0, LGPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=3.2
-TERMUX_PKG_REVISION=4
+TERMUX_PKG_REVISION=0
 
-TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/gnucobol/gnucobol-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=3bb48af46ced4779facf41fdc2ee60e4ccb86eaa99d010b36685315df39c2ee2
-##TERMUX_PKG_SRCURL=git+https://github.com/alexbodn/gnucobol-3.2.git
-##TERMUX_PKG_GIT_BRANCH="termux"
+TERMUX_PKG_SRCURL=git+https://github.com/alexbodn/gnucobol-3.2.git
+TERMUX_PKG_GIT_BRANCH="termux"
 
 TERMUX_INSTALL_DEPS=true
 
 TERMUX_PKG_DEPENDS="json-c, libgmp, libdb, libxml2, ncurses"
-
-#TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686, x86_64"
-#TERMUX_PKG_DEPENDS="libgmp, libdb, ncurses"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 	--with-db
